05d3179fa1548a50741f2d55650e26dc621db324
BRUNO RAFAEL FILGUEIRA DIAS <bruno.dias@viavarejo.com.br>
2026-08-20 08:54:13 -0300

    implementando o application e o factory homeviewmodel

--- app/src/main/java/com/brunorafael/taskflow/TaskFlowApp.kt
+++ app/src/main/java/com/brunorafael/taskflow/TaskFlowApp.kt
@@ -1,10 +1,18 @@
 package com.brunorafael.taskflow
 
+//import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import com.brunorafael.taskflow.ui.screens.home.HomeScreen
+import com.brunorafael.taskflow.ui.viewmodel.HomeViewModelFactory
 
 @Composable
 fun TaskFlowApp() {
+    val application = LocalContext.current.applicationContext as TaskFlowApplication
+    val factory = HomeViewModelFactory(
+        taskRepository = application.taskRepository
+    )
+//    val viewModel: HomeViewModel = viewModel()
     HomeScreen()
 }
 
